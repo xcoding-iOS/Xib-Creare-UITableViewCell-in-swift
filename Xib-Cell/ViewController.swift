99bb7d218ec415abf939e0234fc5bb4a8b416eec
Giuseppe Sapienza <sapienza.giuseppe93@gmail.com>
2019-05-27 09:14:24 +0200

    Initial Commit

---
+++ Xib-Cell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xib-Cell
+//
+//  Created by Giuseppe Sapienza on 27/05/2019.
+//  Copyright © 2019 Giuseppe Sapienza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
